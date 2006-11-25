cb14e205dd9655867c10a72c569b8ded62a6abe9
Deniz Yuret <denizyuret@gmail.com>
2006-11-25 10:03:25 +0000

    *** empty log message ***

--- features.pl
+++ features.pl
@@ -1,18 +1,74 @@
 #!/usr/bin/perl -w
 use strict;
 
+sub mypush {
+    my ($a, $x) = @_;
+    for my $ai (@$a) {
+	return if $x eq $ai;
+    }
+    push @$a, $x;
+}
+
+my $mode;
+if (@ARGV and $ARGV[0] =~ /^\d+$/) {
+    $mode = $ARGV[0];
+} else {
+    $mode = 1;
+}
+
+# input comes with 21 letter windows around the target with all
+# lowercase letters except for Turkish letters represented with
+# uppercase.
+
+# What we print depends on the mode:
+# Bit 0: print all letters
+my $PRINT_ALL = (1<<0);
+# Bit 1: print replace consonants with B
+my $PRINT_NO_CONSONANTS = (1<<1);
+# Bit 2: print replace vowels with A
+my $PRINT_NO_VOWELS = (1<<2);
+# Bit 3: keep caps on the left
+my $KEEP_LEFT_CAPS = (1<<3);
+# Bit 4: keep caps on the right
+my $KEEP_RIGHT_CAPS = (1<<4);
+
 while(<>) {
     my ($class, $str) = /^(\d) (.+)$/;
-    $str =~ s/ /_/g;
-    substr($str, 10, 1, 'X');
-    print $class;
-    for my $l (0 .. 9) {
-	for my $r (0 .. 9) {
-	    next if $l == 0 and $r == 0;
-	    my $val = substr($str, 10 - $l, $l + $r + 1);
-	    print " $val";
-	    last if substr($str, 10+$r, 1) eq '_';
+
+    # Convert sequences of non-alnum characters to '_'
+    my $pre = substr($str, 0, 10);
+    $pre =~ s/\W+/_/g;
+    my $post = substr($str, 11, 10);
+    $post =~ s/\W+/_/g;
+
+    # The base pattern has $l letters on the left and $r letters on
+    # the right
+
+    my @ft = ();
+
+    for my $l (0 .. length($pre)) {
+	my $a = substr($pre, -$l, $l);
+	if (($mode & $KEEP_LEFT_CAPS) == 0) { $a = lc($a); }
+	for my $r (0 .. length($post)) {
+	    next if $l+$r == 0;	# everybody has 'X'
+	    my $b = substr($post, 0, $r);
+	    if (($mode & $KEEP_RIGHT_CAPS) == 0) { $b = lc($b); }
+	    my $p = $a . 'X' . $b;
+	    if ($mode & $PRINT_ALL) { 
+		mypush(\@ft, $p);
+	    }
+	    if ($mode & $PRINT_NO_CONSONANTS) {
+		my $nc = $p;
+		$nc =~ s/[^aeiouAEIOUX_]/B/g;		
+		mypush(\@ft, $nc);
+	    }
+	    if ($mode & $PRINT_NO_VOWELS) {
+		my $nv = $p;
+		$nv =~ s/[aeiouAEIOU]/A/g;
+		mypush(\@ft, $nv);
+	    }
 	}
     }
+    print join(' ', $class, @ft);
     print "\n";
 }
